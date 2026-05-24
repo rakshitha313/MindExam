36cd010ec2280b57256d4fe9ac3e8f393f6a25ab
Rakshitha <rakshithabr510@gmail.com>
2026-05-24 19:06:45 +0530

    Configured MySQL database

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mindexam_db',
-        'USER': 'root',
-        'PASSWORD': 'root123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
